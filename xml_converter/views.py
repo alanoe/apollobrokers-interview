d11a66b1c1a5f04a2ef7435fc6001acd653a3bd4
Alan Evangelista <alan.vitor@gmail.com>
2021-01-30 20:25:05 -0300

    Add form to XML file upload web page and handle XML-JSON conversion in backend

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -1,10 +1,18 @@
-from django.http import JsonResponse
+# 3rd party imports
+from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render
+
+# app imports
+from .converter import convert_xml_to_dict
 
 
 def upload_page(request):
     if request.method == 'POST':
-        # TODO: Convert the submitted XML file into a JSON object and return to the user.
-        return JsonResponse({})
+        # as there is a single file field in the form, there is no need to create a Django form
+        # to validate it
+        if "file" not in request.FILES:
+            return HttpResponseBadRequest("No XML file was chosen")
+        file_content = request.FILES["file"].file.read()
+        return JsonResponse(convert_xml_to_dict(file_content))
 
     return render(request, "upload_page.html")
